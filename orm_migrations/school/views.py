ee3f33ae650e3dbb742c6a94994dc3bf25d52299
Danil Beliaev <dbeliaev18@gmail.com>
2025-11-10 15:12:46 +0300

    update files

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -1,15 +1,16 @@
-from django.views.generic import ListView
 from django.shortcuts import render
-
 from .models import Student
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    # Получаем студентов с упорядочиванием по группе
+    # и предзагружаем связанных учителей для оптимизации
+    students = Student.objects.order_by('group').prefetch_related('teachers')
+
+    context = {
+        'object_list': students  # передаем в контекст под ключом object_list
+    }
 
     return render(request, template, context)
